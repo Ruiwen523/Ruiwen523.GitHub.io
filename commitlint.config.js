dd7be3eb78c4dc4e854c6751274c60adfd6bf790
Ruiwen523 <ruiwen523@gmail.com>
2026-05-17 21:37:32 +0800

    chore: 調整允許提交的前綴格式, 並允許來自main的pr與忽略上游追蹤的develop分支pull歷程。

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,29 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  ignores: [
+    (message) =>
+      /^Merge pull request #\d+ from Ruiwen523\/develop$/.test(message)
+  ],
   rules: {
     'type-enum': [
       2,
       'always',
       [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'perf',
-        'test',
+        'add',
         'build',
         'ci',
         'chore',
+        'draft',
+        'docs',
+        'remove',
+        'feat',
+        'fix',
+        'perf',
+        'test',
+        'refactor',
         'revert',
-        'wip',
-        'draft'
+        'style',
+        'wip'
       ]
     ]
   }
